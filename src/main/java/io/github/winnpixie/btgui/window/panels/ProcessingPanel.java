18110403b79637c0d85ac9626d6d7803a6eff608
Hannah <salvadin2@gmail.com>
2023-05-14 06:47:29 -0400

    add more logging

--- src/main/java/io/github/winnpixie/btgui/window/panels/ProcessingPanel.java
+++ src/main/java/io/github/winnpixie/btgui/window/panels/ProcessingPanel.java
@@ -4,18 +4,16 @@ import io.github.winnpixie.btgui.BuildToolsGUI;
 import io.github.winnpixie.btgui.config.BuildToolsOptions;
 import io.github.winnpixie.btgui.config.ProgramOptions;
 import io.github.winnpixie.btgui.utilities.IOHelper;
-import io.github.winnpixie.btgui.utilities.JARHelper;
+import io.github.winnpixie.btgui.window.JTextFieldWithPlaceholder;
 
 import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
-import java.util.Map;
 
 public class ProcessingPanel extends JPanel {
-    private final JTextField commandToRun = new JTextField();
-    private final JButton prepareBtn = new JButton("Prepare");
+    private final JTextField commandToRun = new JTextFieldWithPlaceholder("Click \"Preview\" to preview what BuildTools will run.");
+    private final JButton prepareBtn = new JButton("Preview");
     private final JButton runBtn = new JButton("Run");
     private final JTextArea outputLog = new JTextArea();
 
@@ -26,8 +24,14 @@ public class ProcessingPanel extends JPanel {
         this.populateWithComponents();
     }
 
-    private void appendToOutput(String msg) {
-        outputLog.append(msg);
+    private void setOutputText(String text) {
+        outputLog.setText(text);
+        if (!text.isEmpty()) BuildToolsGUI.LOGGER.info(text);
+    }
+
+    private void appendToLog(String text) {
+        outputLog.append(String.format("%s%n", text));
+        if (text.isEmpty()) BuildToolsGUI.LOGGER.info(text);
     }
 
     private void populateWithComponents() {
@@ -41,32 +45,40 @@ public class ProcessingPanel extends JPanel {
         super.add(prepareBtn);
 
         runBtn.setBounds(500, 0, 100, 20);
-        runBtn.addActionListener(e -> {
+        runBtn.addActionListener(e -> new Thread(() -> {
+            setOutputText(""); // Clear output log
+
+            // Download BuildTools from SpigotMC Jenkins
             File buildToolsFile = new File(BuildToolsGUI.CURRENT_DIRECTORY, "BuildTools.jar");
             if (ProgramOptions.downloadBuildTools) {
                 try {
-                    BuildToolsGUI.LOGGER.info("Downloading BuildTools.jar");
-                    appendToOutput("Downloading BuildTools.jar from https://hub.spigotmc.org/");
+                    appendToLog("Downloading BuildTools.jar from https://hub.spigotmc.org/");
                     byte[] btData = IOHelper.getBytes("https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar");
                     Files.write(buildToolsFile.toPath(), btData);
                 } catch (IOException ex) {
+                    appendToLog("Failed to download or save BuildTools.jar, short-circuiting!");
+
                     ex.printStackTrace();
                     return;
                 }
             }
 
+            // Create Run Directory
             File runDir = new File(BuildToolsGUI.CURRENT_DIRECTORY, "run");
             if (ProgramOptions.isolateRuns) {
                 runDir = new File(BuildToolsGUI.CURRENT_DIRECTORY, String.format("run-%d", System.currentTimeMillis()));
             }
 
             if (!runDir.exists()) runDir.mkdir();
+            appendToLog(String.format("Created run directory @ %s", runDir.getAbsolutePath()));
 
             try {
-
+                appendToLog("Copying BuildTools.jar to run directory");
+                // Copy BuildTools to Run directory
                 File buildToolsCopy = new File(runDir, "BuildTools.jar");
                 Files.copy(buildToolsFile.toPath(), buildToolsCopy.toPath());
 
+                // Build process
                 ProcessBuilder procBuilder = new ProcessBuilder()
                         .command(String.format("\"%s%cbin%cjava\"", ProgramOptions.javaHome, File.separatorChar, File.separatorChar),
                                 String.format("-Xms%s", ProgramOptions.heapSize),
@@ -79,34 +91,29 @@ public class ProcessingPanel extends JPanel {
                 procBuilder.command().addAll(BuildToolsOptions.buildArguments());
                 procBuilder.environment().put("JAVA_HOME", ProgramOptions.javaHome);
 
+                appendToLog("Running BuildTools.jar");
+                appendToLog("BuildTools Command:");
+                appendToLog(String.join(" ", BuildToolsOptions.buildArguments()));
+                appendToLog("");
+                appendToLog("Full Command:");
+                appendToLog(String.join(" ", procBuilder.command()));
+                appendToLog("Please wait...");
+
+                // Start process and wait to finish
                 Process proc = procBuilder.start();
-            } catch (IOException ex) {
+                proc.waitFor();
+            } catch (IOException | InterruptedException ex) {
                 ex.printStackTrace();
-            }
+            } finally {
+                appendToLog("Completed (hopefully) without errors!");
 
-            if (ProgramOptions.deleteRunOnFinish) {
-                IOHelper.delete(runDir);
-            }
-
-            // TODO: Remove loading BT directly into this JVM, add functionality to run from a specified JAVA_HOME
-            if (1 == 1) return; // Short-circuited for testing
-            try {
-                BuildToolsGUI.LOGGER.info("Loading BuildTools classes");
-                appendToOutput("Loading BuildTools into current JVM...");
-
-                Map<String, Class<?>> classes = JARHelper.loadClasses(buildToolsFile);
-
-                try {
-                    BuildToolsGUI.LOGGER.info("Running BuildTools");
-                    classes.get("org.spigotmc.builder.Bootstrap").getDeclaredMethod("main", String[].class)
-                            .invoke(null, new Object[]{BuildToolsOptions.buildArguments().toArray(new String[0])});
-                } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException ex) {
-                    ex.printStackTrace();
+                // (optionally) Clean up after ourselves :)
+                if (ProgramOptions.deleteRunOnFinish) {
+                    appendToLog("Cleaning up...");
+                    IOHelper.delete(runDir);
                 }
-            } catch (IOException ex) {
-                ex.printStackTrace();
             }
-        });
+        }).start());
         super.add(runBtn);
 
         outputLog.setBounds(0, 20, 620, 420);
